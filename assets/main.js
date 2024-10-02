915ba122d7c551acd7519b2866bcf41234c20b12
Gurban <gurba@Big_Man.(none)>
2024-10-02 11:45:36 +0400

    Fixed some erros and added error handling

--- assets/main.js
+++ assets/main.js
@@ -2,27 +2,42 @@ async function LoadingIntoHTML(Url , table){
     
     const tableHead = table.querySelector('thead');
     const tableBody = table.querySelector('tbody');
-    const response = await fetch(Url);
-    const { headers , rows } = await response.json(); // Destructuring object above line
+    try {
+        const response = await fetch(Url); // Update the path as needed
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const { headers , rows } = await response.json(); // Destructuring object above line
+        tableHead.innerHTML= "<tr></tr>";
+        tableBody.innerHTMl="";
 
 
-    tableHead.innerHTML= "<tr></tr>";
-    tableBody.innerHTMl="";
-    for (text of headers){
+        for (text of headers){
         const headerElement = document.createElement("th");
         headerElement.textContent = text;
         tableHead.querySelector('tr').appendChild(headerElement);
+
+        } 
         
-    }
+        
 
    
 
+
+    
+}
+
+
+     catch (error) {
+        console.error('Fetch error:', error);
+    }
 
 }
 
 
 
-  LoadingIntoHTML("./main.json" , document.querySelector('table'));
+
+  LoadingIntoHTML("./assets/main.json" , document.querySelector('table'));
 
 
 
